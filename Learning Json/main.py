6bdbf37f29542358fb0b1d4667c7bb337e7f29a8
Michael Ariyo <ariyoajolayo@gmail.com>
2026-08-28 10:51:35 +0100

    Started Lesrning Json in python

---
+++ Learning Json/main.py
@@ -0,0 +1,31 @@
+# JSON = Javascript Object Notation
+
+import json 
+
+""" json_string = '''
+    {
+            "students": [
+            {
+                    "id": 1,
+                    "name": "Tim",
+                    "age": 21,
+                    "full-time": true
+            },
+            {
+                    "id": 2,
+                    "name": "Joe",
+                    "age": 33,
+                    "full-time" : false
+            }
+        ]
+    }
+''' """
+
+""" data = json.loads(json_string)
+data['test'] = True
+
+new_json = json.dumps(data, indent=4, sort_keys=True)
+print(new_json) """
+
+with open("data.json", "r"):
+        pass
