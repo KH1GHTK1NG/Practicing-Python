8e7363d213eec89259981fc694396a2c2e68a1fd
Michael Ariyo <ariyoajolayo@gmail.com>
2026-08-31 10:44:21 +0100

    Corrected Some Bugs

--- Learning Json/Semi Project Json/analysis.py
+++ Learning Json/Semi Project Json/analysis.py
@@ -1,14 +1,14 @@
 import json
 from pprint import pprint
 
-with open("Learning Json\Semi Project Json\catalog.json", "r") as file:
+with open("Learning Json\\Semi Project Json\\catalog.json", "r") as file:
     catalog = json.load(file)
 
 #for product in catalog["product"]:
 #   print(product.get("sale_price"))
-catalog["product"][0].get("size").append("extra large")
+catalog["product"][0].get("sizes").append("extra large")
 
-with open("Learning Json\Semi Project Json\catalog.json", "w") as file:
+with open("Learning Json\\Semi Project Json\\catalog.json", "w") as file:
     json.dump(catalog, file, indent=2)
 
 #pprint(catalog["product"][0], width=40)
