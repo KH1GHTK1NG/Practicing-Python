9542a0ccae0f82b4e0c6d30860304d0de2209711
Michael Ariyo <ariyoajolayo@gmail.com>
2026-08-31 09:13:08 +0100

    Made a new json project, to practice json

---
+++ Learning Json/Semi Project Json/analysis.py
@@ -0,0 +1,13 @@
+import json
+from pprint import pprint
+
+with open("Learning Json\Semi Project Json\catalog.json", "r") as file:
+    catalog = json.load(file)
+
+#for product in catalog["product"]:
+#   print(product.get("sale_price"))
+
+with open("Learning Json\Semi Project Json\catalog.json", "w") as file:
+    json.dump(catalog, file, indent=2)
+
+#pprint(catalog["product"][0], width=40)
